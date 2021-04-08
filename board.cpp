0a86eae1bd3e6000bd5e760638b31273f0a6877c
Fandi <63651972+JanuarAfandi@users.noreply.github.com>
2021-04-08 22:52:28 +0700

    Add files via upload

---
+++ board.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include "board.h"
+using namespace std;
+class board {
+private:
+public:
+};
---
+++ gamemanager.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include "gamemanager.h"
+using namespace std;
+class board {
+private:
+public:
+};
