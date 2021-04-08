73a3c5cd4bd2f9ae1f7c741fe81a68658d2bc958
pdcm-mar20 <63573499+pdcm-mar20@users.noreply.github.com>
2021-04-08 22:46:04 +0700

    Add files via upload

---
+++ player.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+class Player {
+private:
+	string name;
+	int score;
+public:
+	void setName(string);
+	string getName();
+	void addedScore(int);
+	void lessScore(int);
+	int getScore();
+};
